da8aa53b51d309a82996dfff7e62db2471e4c1e9
godhyzzang <amksh0914@gmail.com>
2023-10-06 11:06:32 +0900

    4회차-실습 2 : 사칙연산 계산기
    4회차-실습 2 : 사칙연산 계산기

--- main.c
+++ main.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-	int x, y, z, m;
-	int a, b, c;
-	x = 2;
-	z = 1;
-	a = 3;
-	b = 4;
-	c = 5;
+	int num1, num2;
+	
+	printf("INPUT two int  : ");
+	scanf_s("%i %i", &num1, &num2);
+	
 
-	/*y = a * x ^ 2 + b * x + c;*/
-	//12 + 8+5
-	y = a * x *x+ b * x + c; //25
-	m = (x + y + z) / 3; //9
-		//정수말고 진짜 몫을 구해야할때는 float형변환 필요?
-
-	printf("y=%d, m=%d", y, m);
+	printf("%i + %i = %i \n",num1,num2,num1+num2);
+	printf("%i - %i = %i \n", num1, num2, num1 - num2);
+	printf("%i * %i = %i \n", num1, num2, num1 * num2);
+	printf("%i / %i = %i \n", num1, num2, num1 / num2);
+	printf("%i %% %i = %i \n", num1, num2, num1 % num2);
+	
+	return 0; 
+	
 
 }
