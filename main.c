d4fdd7df531d59b443e185fc557617942b6e698f
godhyzzang <amksh0914@gmail.com>
2023-10-06 11:45:14 +0900

    4회차 실습 5 : 1의 개수 계산
    4회차 실습 5 : 1의 개수 계산

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-int main(void) {
-	int year;
-	
-	
-	printf("INPUT year  : ");
-	scanf_s("%i", &year);
+int main(void){
+		unsigned int x; 
+		int b;
 
-	if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
-		printf("LEAP YEAR");
-	else
-		printf("NOT leap year");
+		printf("input a number : ");
+		scanf_s("%ui", &x);
 
-	
-	return 0; 
+		for (b = 0; x != 0; x >>=1 )
+		{
+			if (x & 1) {
+				b++;
+			}
+		}
+		printf("The result is : %i\n", b);
+
+		return 0;
+
+
 	
 
 }
