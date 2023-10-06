b2a1ee3ff75db458af8f9bbae406c90e452ba716
godhyzzang <amksh0914@gmail.com>
2023-10-06 11:15:38 +0900

    4회차 실습 3 : 나누기/나머지 연산자
    4회차 실습 3 : 나누기/나머지 연산자

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-	int num1, num2;
+	int input_s;
+	int m, s;
 	
-	printf("INPUT two int  : ");
-	scanf_s("%i %i", &num1, &num2);
+	printf("INPUT sec  : ");
+	scanf_s("%i", &input_s);
+	s = input_s % 60;
+	m = input_s / 60;
 	
 
-	printf("%i + %i = %i \n",num1,num2,num1+num2);
-	printf("%i - %i = %i \n", num1, num2, num1 - num2);
-	printf("%i * %i = %i \n", num1, num2, num1 * num2);
-	printf("%i / %i = %i \n", num1, num2, num1 / num2);
-	printf("%i %% %i = %i \n", num1, num2, num1 % num2);
+	printf("%i m : %i s \n",m,s);
 	
 	return 0; 
 	
