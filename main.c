55fb083e2a8916b2f6043f68d5aa89d8c698ecdc
godhyzzang <amksh0914@gmail.com>
2023-10-06 11:47:26 +0900

    4회차 실습 6 : 시분초 표시
    4회차 실습 6 : 시분초 표시

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-int main(void){
-		unsigned int x; 
-		int b;
+int main(void) {
+	int input_s;
+	int h, m, s;
 
-		printf("input a number : ");
-		scanf_s("%ui", &x);
-
-		for (b = 0; x != 0; x >>=1 )
-		{
-			if (x & 1) {
-				b++;
-			}
-		}
-		printf("The result is : %i\n", b);
-
-		return 0;
+	printf("INPUT sec  : ");
+	scanf_s("%i", &input_s);
+	s = input_s % 3600 % 60;
+	m = input_s % 3600 / 60;
+	h = input_s / 3600;
 
 
-	
+	printf("%i h : %i m : %i s \n", h, m, s);
+
+	return 0;
+
 
 }
